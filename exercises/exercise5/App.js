35c60008aa7f6b97a2f7b7bcdc6b0ec29285a500
shahih25 <hmshahid@asu.edu>
2024-04-07 00:35:31 -0700

    test commit

--- exercises/exercise5/App.js
+++ exercises/exercise5/App.js
@@ -10,9 +10,12 @@ import {
   TouchableOpacity,
   View
 } from "react-native";
+import Notification from "./notifications";
 
 export default class App extends Component {
   state = {
+    notify: false,
+    message: null,
     currentLocation: null,
     location: null,
     poi1: {
@@ -38,7 +41,11 @@ export default class App extends Component {
 
   async getLocation() {
     let loc = await Location.getCurrentPositionAsync();
-    this.setState({ location: loc, currentLocation: loc });
+    this.setState({ location: loc, currentLocation: loc, notify: true, message: "Changed to You" });
+  }
+
+  toggleNotification = () => {
+    this.setState({ notify: !this.state.notify });
   }
 
   showMap() {
@@ -63,6 +70,13 @@ export default class App extends Component {
   }
 
   render() {
+    const notify = this.state.notify
+      ? <Notification
+          autoHide
+          message={this.state.message}
+          onClose={this.toggleNotification}
+        />
+    : null;
     return (
       <View style={styles.container}>
         {this.showMap()}
@@ -70,13 +84,15 @@ export default class App extends Component {
           <TouchableOpacity onPress={() => this.getLocation()} style={styles.btn}>
             <Text>YOU</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.setState({ ...this.state, currentLocation: this.state.poi1 })} style={styles.btn}>
+          <TouchableOpacity onPress={() => this.setState({ ...this.state, currentLocation: this.state.poi1, notify: true, message: "Changed to POI1" })} style={styles.btn}>
             <Text>POI1</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.setState({ ...this.state, currentLocation: this.state.poi2 })} style={styles.btn}>
+          <TouchableOpacity onPress={() => this.setState({ ...this.state, currentLocation: this.state.poi2, notify: true, message: "Changed to POI2" })} style={styles.btn}>
             <Text>POI2</Text>
           </TouchableOpacity>
         </View>
+
+        {notify}
       </View>
     );
   }
@@ -88,7 +104,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "stretch",
     justifyContent: "center",
-    marginRight: 10
+    marginTop: 30,
+    marginBottom: 10
   },
   rowContainer: {
     flex: 1,
@@ -107,74 +124,3 @@ const styles = StyleSheet.create({
     marginTop: 10
   }
 });
-
-
-// import React, { Component } from "react";
-// import * as Location from "expo-location";
-// import MapView from "react-native-maps";
-// import { Marker } from "react-native-maps";
-// import {
-//   Dimensions,
-//   StyleSheet,
-//   Text, // These are not used yet, but will likely be useful later in the exercise
-//   TouchableOpacity,
-//   View
-// } from "react-native";
-
-// export default class App extends Component {
-//   state = {
-//     location: null
-//   };
-
-//   async componentDidMount() {
-//     const permission = await Location.requestForegroundPermissionsAsync();
-//     if (permission.granted) {
-//       this.getLocation();
-//     }
-//   }
-
-//   async getLocation() {
-//     let loc = await Location.getCurrentPositionAsync();
-//     this.setState({ location: loc, currentLocation: loc });
-//   }
-
-//   render() {
-//     return this.state.location ? (
-//       <MapView
-//         style={styles.map}
-//         region={{
-//           latitude: this.state.location.coords.latitude,
-//           longitude: this.state.location.coords.longitude,
-//           latitudeDelta: 0.09,
-//           longitudeDelta: 0.04,
-//         }}
-//       >
-//         <Marker
-//           coordinate={this.state.location.coords}
-//           title={"User Location"}
-//           description={"You are here!"}
-//           image={require("./assets/you-are-here.png")}
-//         />
-//       </MapView>
-//     ) : null;
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   rowContainer: {
-//     flex: 1,
-//     flexDirection: "row",
-//     justifyContent: "space-around",
-//   },
-//   map: {
-//     flex: 7,
-//     width: Dimensions.get("window").width,
-//     height: Dimensions.get("window").height,
-//   }
-// });
